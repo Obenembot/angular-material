5d249b5a74c3431ff3f0220aaf1204e412f2b5d9
Obenembot <embottabi@gmail.com>
2022-02-01 01:36:49 +0200

    added drop down menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatButtonModule} from "@angular/material/button";
+import {HomeComponent} from "./home/home.component";
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AboutComponent} from "./about/about.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopMenuComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
